07c8f032c55bf7e9cbe515d356cb4dd2d8419396
BenoitArbelot <benoit.arbelot@gmail.com>
2022-03-30 18:23:56 +0200

    Updated Ripples controllable.

--- Assets/Scripts/RipplesComputeControllerControllable.cs
+++ Assets/Scripts/RipplesComputeControllerControllable.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class RipplesComputeControllerControllable : Controllable
 {
-    [OSCProperty] public int updatePerSeconds = 60;
     [OSCProperty] public Vector2 ripplesSize = Vector2.one;
     [OSCProperty] public float ripplesScale = 5.0f;
-    [OSCProperty] public float ripplesAttenuation = 0.01f;
+    [OSCProperty] [Range(0, 1)] public float ripplesAttenuation = 0.01f;
     [OSCProperty] public Vector3 ripplesLightDirection = new Vector3(.2f, -.5f, .7f);
     [OSCProperty] public float ripplesSpecular = 10.0f;
     [OSCProperty] public float ripplesSpecularPower = 32.0f;
